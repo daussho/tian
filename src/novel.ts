1db058e9ac940bd07dbaeddbb057fbb120d20b16
Abdul Hafizh Firdaus <abdulhafizh9@gmail.com>
2021-05-17 11:19:40 +0700

    WIP: extract categories and tags

--- src/novel.ts
+++ src/novel.ts
@@ -15,10 +15,12 @@ export class Client {
     const $ = cheerio.load(html);
 
     const novelList = $("div.media").map((_i, novelHtml) => {
-      const [categories, tags] = $(
-        ".list-inline.text-center",
-        novelHtml
-      ).toArray();
+      const [categories, tags] = $(".list-inline.text-center", novelHtml)
+        .map((_i, el) => {
+          return $("li", el).map((_i, e) => $(e).text().toString());
+        })
+        .toArray();
+
       return {
         title: $("h4.media-title", novelHtml).text(),
         image: $("img", novelHtml).attr("src"),
